d4a982ed443a80c9d99b58cc74a35b0a6597e5f2
Asakundwi Mavhungu <mavhasa17@gmail.com>
2023-03-31 01:06:43 +0200

    added 'sourceProcessed' to store the binary image

--- imageProcessor.h
+++ imageProcessor.h
@@ -15,6 +15,7 @@ namespace MVHASA001
 			int width, height;
 			char *id;
 			std::vector<std::vector<unsigned char>> * source;
+			std::vector<std::vector<unsigned char>> sourceProcessed;
 			int sourceSize;
 			std::string comment;
 
