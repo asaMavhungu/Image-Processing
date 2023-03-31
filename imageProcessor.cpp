ee89a8f7d61f3a844d2ca494e9229eccbe0d4f37
Asakundwi Mavhungu <mavhasa17@gmail.com>
2023-03-31 23:30:24 +0200

    Moved all implementations into namespace

--- imageProcessor.cpp
+++ imageProcessor.cpp
@@ -8,215 +8,218 @@
 #include <queue>
 
 using namespace std;
-using namespace MVHASA001;
 
-void MVHASA001::PGMimageProcessor::setImage(string filename)
+namespace MVHASA001
 {
-	this->sourceName = filename;
-}
 
-void MVHASA001::PGMimageProcessor::readImage(string filename, unsigned char threshold)
-{
-	std::ifstream in(filename, std::ios::binary);
-
-	std::string format, commentBuffer, comment, line;
-	int width = 0, height = 0, max_value = 0;
-
-	// Read the first line to get the format
-	std::getline(in, format);
-
-	// Read any comment lines (starting with #)
-	while (std::getline(in, commentBuffer)) 
+	void PGMimageProcessor::setImage(string filename)
 	{
-		if (commentBuffer[0] != '#') 
-		{
-			break;
-		}
-		comment = comment + commentBuffer;
+		this->sourceName = filename;
 	}
 
-	// The last line that doesn't have '#' will be saved to comment
-	std::istringstream iss(commentBuffer);
-	iss >> width >> height;
-
-
-	std::getline(in, line);
-	std::istringstream iss2(line);
-	iss2 >> max_value;
-
-	// Remove whitespace
-	in >> std::ws;
-
-	// Read pixel data to 2D vector
-    std::vector<std::vector<unsigned char>> pixels(height, std::vector<unsigned char>(width));
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            in.read((char*)&pixels[i][j], 1);
-        }
-    }
-	// TODO make sure sont need original data
-	// this->source = pixels; // save original sorce
-
-    // Set background and forground
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-			// set dummy black and white
-			// will use real colors when doing flood fill
-            if (pixels[i][j] > threshold)
-                pixels[i][j] = 254;
-            else
-				pixels[i][j] = 1;
-        }
-    }
-
-
-	this->comment = comment;
-	this->source = std::move(pixels); // move the pixel data to the class instance
-	this->height = height;
-	this->width = width;
-	this->sourceSize = width * height;
-	in.close();
-}
-
-
-void MVHASA001::PGMimageProcessor::process(vector<vector<unsigned char>> &image, int threshold,int width, int height)
-{
-	int row = 0;
-	int col = 0;
-
-	for (int y = 0; y < height; y++)
+	void PGMimageProcessor::readImage(string filename, unsigned char threshold)
 	{
-		for (int x = 0; x < width; x++)
+		std::ifstream in(filename, std::ios::binary);
+
+		std::string format, commentBuffer, comment, line;
+		int width = 0, height = 0, max_value = 0;
+
+		// Read the first line to get the format
+		std::getline(in, format);
+
+		// Read any comment lines (starting with #)
+		while (std::getline(in, commentBuffer)) 
 		{
-			image[y][x] = (image[y][x] > threshold) ? 255 : 0;
+			if (commentBuffer[0] != '#') 
+			{
+				break;
+			}
+			comment = comment + commentBuffer;
 		}
 
-	}
+		// The last line that doesn't have '#' will be saved to comment
+		std::istringstream iss(commentBuffer);
+		iss >> width >> height;
 
-}
 
-// Check if the given row and column are within bounds
-bool isSafe(int row, int col, int numRows, int numCols) 
-{
-    return (row >= 0 && row < numRows && col >= 0 && col < numCols);
-}
+		std::getline(in, line);
+		std::istringstream iss2(line);
+		iss2 >> max_value;
 
-int MVHASA001::PGMimageProcessor::extractComponents(unsigned char threshold, int minValidSize)
-{
-	using namespace std;
+		// Remove whitespace
+		in >> std::ws;
 
-	readImage(this->sourceName, threshold);
-
-    int numRows = this->height;
-    int numCols = this->width;
-    int numComponents = 0;
-    vector<vector<bool>> visited(numRows, vector<bool>(numCols, false));
-    queue<pair<int, int>> q;
-
-	
-    for (int i = 0; i < numRows; i++) 
-	{
-        for (int j = 0; j < numCols; j++) 
+		// Read pixel data to 2D vector
+		std::vector<std::vector<unsigned char>> pixels(height, std::vector<unsigned char>(width));
+		for (int i = 0; i < height; i++)
 		{
+			for (int j = 0; j < width; j++)
+			{
+				in.read((char*)&pixels[i][j], 1);
+			}
+		}
+		// TODO make sure sont need original data
+		// this->source = pixels; // save original sorce
 
-            if (!visited[i][j]) {
-                q.push(make_pair(i, j));
-                visited[i][j] = true;
-                numComponents++;
-				int size = 0;
-
-                // Start new connected component
-				// TODO make array to hold the pixels(x, y) changed to store in component
-				ConnectedComponent *comp = new ConnectedComponent(numComponents);
-				//cout << comp->getSize() << size << endl;
-                while (!q.empty()) 
-				{
-                    pair<int, int> p = q.front();
-                    q.pop();
-                    int row = p.first;
-                    int col = p.second;
-
-					std::pair<int,int> p1 = std::make_pair(i,j);
-					*comp += p1;
-
-					++size;
-
-
-                    // Check neighbor pixels
-                    if (isSafe(row-1, col, numRows, numCols) && !visited[row-1][col]
-						&& this->source[row-1][col] == this->source[row][col]) 
-					{
-                        q.push(make_pair(row-1, col));
-                        visited[row-1][col] = true;
-						//cout << "queued" << row-1 << col<< endl;
-                    }
-                    if (isSafe(row+1, col, numRows, numCols) && !visited[row+1][col]
-						&& this->source[row+1][col] == this->source[row][col]) 
-					{
-                        q.push(make_pair(row+1, col));
-                        visited[row+1][col] = true;
-						//cout << "queued" << row+1 << col<< endl;
-                    }
-                    if (isSafe(row, col-1, numRows, numCols) && !visited[row][col-1]
-						&& this->source[row][col-1] == this->source[row][col]) 
-					{
-                        q.push(make_pair(row, col-1));
-                        visited[row][col-1] = true;
-						//cout << "queued" << row << col-1<< endl;
-                    }
-                    if (isSafe(row, col+1, numRows, numCols) && !visited[row][col+1]
-						&& this->source[row][col+1] == this->source[row][col]) 
-					{
-                        q.push(make_pair(row, col+1));
-                        visited[row][col+1] = true;
-						//cout << "queued" << row <<col+1<< endl;
-                    }
-                }
-				if (size <= minValidSize) 
-				{
-					//cout << "did not make component " << size << endl;
-					// TODO make and not make components
-					delete comp;
-					--numComponents; 
-				}
-				else 
-				{
-					cout << "made component " << size << endl; 
-					comp->setSize(size);
-					cout << comp << endl;
-					this->addComponent(*comp);
-					//cout << this->components[0] <<" ONE\n";
-				}
-				//cout << "queue empty" << endl;
-            }
-        }
-    }
-	
-	
-	cout << this->getComponents()[0] <<" ONE\n";
-	cout << this->getComponents()[2] <<" ONE\n";
-
-    return numComponents;
-}
-
-bool MVHASA001::PGMimageProcessor::writeComponents(const std::string & outFileName)
-{
-	std::ostringstream oss;
-	std::ofstream file(outFileName, std::ios::binary);
-	file << "P5" << std::endl;
-    file << this->width << " " << this->height << std::endl;
-    file << "255" << std::endl;
+		// Set background and forground
+		for (int i = 0; i < height; i++)
+		{
+			for (int j = 0; j < width; j++)
+			{
+				// set dummy black and white
+				// will use real colors when doing flood fill
+				if (pixels[i][j] > threshold)
+					pixels[i][j] = 254;
+				else
+					pixels[i][j] = 1;
+			}
+		}
 
 
-	for (int y = 0; y < this->height; y++) 
-	{
-		file.write(reinterpret_cast<const char*>(this->source[y].data()), this->width);
+		this->comment = comment;
+		this->source = std::move(pixels); // move the pixel data to the class instance
+		this->height = height;
+		this->width = width;
+		this->sourceSize = width * height;
+		in.close();
 	}
 
-	return true;
+
+	void PGMimageProcessor::process(vector<vector<unsigned char>> &image, int threshold,int width, int height)
+	{
+		int row = 0;
+		int col = 0;
+
+		for (int y = 0; y < height; y++)
+		{
+			for (int x = 0; x < width; x++)
+			{
+				image[y][x] = (image[y][x] > threshold) ? 255 : 0;
+			}
+
+		}
+
+	}
+
+	// Check if the given row and column are within bounds
+	bool isSafe(int row, int col, int numRows, int numCols) 
+	{
+		return (row >= 0 && row < numRows && col >= 0 && col < numCols);
+	}
+
+	int PGMimageProcessor::extractComponents(unsigned char threshold, int minValidSize)
+	{
+		using namespace std;
+
+		readImage(this->sourceName, threshold);
+
+		int numRows = this->height;
+		int numCols = this->width;
+		int numComponents = 0;
+		vector<vector<bool>> visited(numRows, vector<bool>(numCols, false));
+		queue<pair<int, int>> q;
+
+		
+		for (int i = 0; i < numRows; i++) 
+		{	
+			for (int j = 0; j < numCols; j++) 
+			{
+				if(this->source[i][j] < threshold)
+					continue;
+				if (!visited[i][j]) {
+					q.push(make_pair(i, j));
+					visited[i][j] = true;
+					numComponents++;
+					int size = 0;
+
+					// Start new connected component
+					// TODO make array to hold the pixels(x, y) changed to store in component
+					ConnectedComponent *comp = new ConnectedComponent(numComponents);
+					//cout << comp->getSize() << size << endl;
+					while (!q.empty()) 
+					{
+						pair<int, int> p = q.front();
+						q.pop();
+						int row = p.first;
+						int col = p.second;
+
+						std::pair<int,int> p1 = std::make_pair(i,j);
+						*comp += p1;
+
+						++size;
+
+
+						// Check neighbor pixels
+						if (isSafe(row-1, col, numRows, numCols) && !visited[row-1][col]
+							&& this->source[row-1][col] == this->source[row][col]) 
+						{
+							q.push(make_pair(row-1, col));
+							visited[row-1][col] = true;
+							//cout << "queued" << row-1 << col<< endl;
+						}
+						if (isSafe(row+1, col, numRows, numCols) && !visited[row+1][col]
+							&& this->source[row+1][col] == this->source[row][col]) 
+						{
+							q.push(make_pair(row+1, col));
+							visited[row+1][col] = true;
+							//cout << "queued" << row+1 << col<< endl;
+						}
+						if (isSafe(row, col-1, numRows, numCols) && !visited[row][col-1]
+							&& this->source[row][col-1] == this->source[row][col]) 
+						{
+							q.push(make_pair(row, col-1));
+							visited[row][col-1] = true;
+							//cout << "queued" << row << col-1<< endl;
+						}
+						if (isSafe(row, col+1, numRows, numCols) && !visited[row][col+1]
+							&& this->source[row][col+1] == this->source[row][col]) 
+						{
+							q.push(make_pair(row, col+1));
+							visited[row][col+1] = true;
+							//cout << "queued" << row <<col+1<< endl;
+						}
+					}
+					if (size <= minValidSize) 
+					{
+						//cout << "did not make component " << size << endl;
+						// TODO make and not make components
+						delete comp;
+						--numComponents; 
+					}
+					else 
+					{
+						comp->setSize(size);
+						this->addComponent(*comp);
+						//cout << this->components[0] <<" ONE\n";
+					}
+					//cout << "queue empty" << endl;
+				}
+			}
+		}
+		
+		
+		return numComponents;
+	}
+
+	bool PGMimageProcessor::writeComponents(const std::string & outFileName)
+	{
+		std::ostringstream oss;
+		std::ofstream file(outFileName, std::ios::binary);
+		file << "P5" << std::endl;
+		file << this->width << " " << this->height << std::endl;
+		file << "255" << std::endl;
+
+
+		for (int y = 0; y < this->height; y++) 
+		{
+			file.write(reinterpret_cast<const char*>(this->source[y].data()), this->width);
+		}
+
+		return true;
+
+	}
+
+
 
 }
+
