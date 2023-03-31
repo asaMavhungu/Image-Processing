0615acfcb53077573ffce82af152ad4a5331f974
Asakundwi Mavhungu <mavhasa17@gmail.com>
2023-04-01 01:15:13 +0200

    added 'getPoint' implemetation

--- connectedComponent.cpp
+++ connectedComponent.cpp
@@ -39,6 +39,11 @@ namespace MVHASA001
 		return *this;
 	}
 
+	std::pair<int,int> ConnectedComponent::getPoint()
+	{	
+		return this->points[0];
+	}
+
 	int MVHASA001::ConnectedComponent::getSize()
 	{
 		return this->size;
