fe4f218c466cffc02dd798ea09656d1a12f4cef5
Asakundwi Mavhungu <mavhasa17@gmail.com>
2023-03-31 18:18:51 +0200

    added implemtation of component+=std::pait

--- connectedComponent.cpp
+++ connectedComponent.cpp
@@ -1,5 +1,13 @@
 #include "connectedComponent.h"
 
-MVHASA001::ConnectedComponent::ConnectedComponent() : id(nullptr), points()
+using namespace MVHASA001;
+
+ConnectedComponent::ConnectedComponent() : id(nullptr), points()
 {
+}
+
+ConnectedComponent &ConnectedComponent::operator+=(const std::pair<int, int> &rhs)
+{
+	this->points.push_back(rhs);
+	return *this;
 }
