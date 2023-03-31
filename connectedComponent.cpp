c9b585f646b1a7d9e4fa01b971c10867dcd01283
Asakundwi Mavhungu <mavhasa17@gmail.com>
2023-03-31 20:22:14 +0200

    fixed up and re-implemented the big 6

--- connectedComponent.cpp
+++ connectedComponent.cpp
@@ -2,12 +2,54 @@
 
 using namespace MVHASA001;
 
-ConnectedComponent::ConnectedComponent() : id(nullptr), points()
+namespace MVHASA001
 {
-}
 
-ConnectedComponent &ConnectedComponent::operator+=(const std::pair<int, int> &rhs)
-{
-	this->points.push_back(rhs);
-	return *this;
+	ConnectedComponent::ConnectedComponent() : id(0), points() // constr
+	{
+	}
+
+	ConnectedComponent::ConnectedComponent(int x) : id(x), points() // constr
+	{
+	}
+
+	ConnectedComponent::~ConnectedComponent() = default; // destruct
+
+	ConnectedComponent::ConnectedComponent(const ConnectedComponent &rhs) : id(rhs.id),  size(rhs.size), points(rhs.points) {} // copy
+
+	ConnectedComponent &ConnectedComponent::operator=(const ConnectedComponent &rhs) // copy assign
+	{
+		if (this == &rhs) return *this;
+		points = rhs.points;
+		return *this;
+	}
+
+	ConnectedComponent::ConnectedComponent(ConnectedComponent && rhs) : points(move(rhs.points)) {}; // move
+
+	ConnectedComponent & ConnectedComponent::operator=(ConnectedComponent &&rhs) 
+	{
+		if (this == &rhs) return *this;
+		points = std::move(rhs.points);
+		return *this;
+	}
+
+	ConnectedComponent &ConnectedComponent::operator+=(const std::pair<int, int> &rhs)
+	{
+		this->points.push_back(rhs);
+		return *this;
+	}
+
+	void MVHASA001::ConnectedComponent::setSize(int i)
+	{
+		this->size = i;
+	}
+
+	std::ostream &operator<<(std::ostream &os, const ConnectedComponent &rhs)
+	{
+		os << "ID: " << rhs.id << ", Size: " << rhs.size;
+		return os;
+	}
+
+
+
 }
