4e8c699e9f3af27315df35fe80e61d20b1c7c87a
Asakundwi Mavhungu <mavhasa17@gmail.com>
2023-04-03 23:31:19 +0200

    Fixed argument parsing

--- driver.cpp
+++ driver.cpp
@@ -1,7 +1,7 @@
 #include "imageProcessor.h"
 #include <iostream>
 #include <cstring>
-
+#include <sstream>
 
 int main(int argc, char* argv[])
 {
@@ -9,6 +9,8 @@ int main(int argc, char* argv[])
 
 
 	int minSize = 3;
+
+	bool maxSet = false;
 	int maxSize = 0;
 	int threshold = 55;
 
@@ -26,6 +28,7 @@ int main(int argc, char* argv[])
 	{
 		if ( strcmp( argv[i], "-s") == 0)
 		{
+			maxSet = true;
 			minSize = std::stoi(argv[i+1]);
 			++i;
 			maxSize = std::stoi(argv[i+2]);
@@ -34,34 +37,24 @@ int main(int argc, char* argv[])
 		if ( strcmp( argv[i], "-t") == 0)
 		{
 			threshold = std::stoi(argv[i+1]);
-			i++;
+			++i;
 		}
 		if ( strcmp( argv[i], "-p") == 0)
 		{
 			printOut = true;
-			i++;
 		}
 		if ( strcmp( argv[i], "-w") == 0)
 		{
 			writeOut = true;
 			outFileName = argv[i+1];
-			i++;
+			++i;
 		}
 	}
 
-
-
-	//std::string inFilename = "chess.pgm";
-
-
-	//PGMimageProcessor asa;
-	//std::string filename3 = "chess.pgm";
-
-	//asa.extractComponents(127, 3);
-	//int maxSize = asa.getSourceSize();
-
-	std::cout << asa.extractComponents(threshold, minSize) << " ------\n";
-	maxSize = asa.getSourceSize();
+	asa.extractComponents(threshold, minSize);
+	if (!maxSet)
+		maxSize = asa.getSourceSize();
+	
 	asa.filterComponentsBySize(minSize, maxSize);
 	if (printOut)
 	{
@@ -75,44 +68,5 @@ int main(int argc, char* argv[])
 		asa.writeComponents(outFileName);
 	}
 
-	std::cout << "Num: " << asa.getComponentCount() << endl;
-
-	std::cout << minSize << " MIN "<< std::endl
-		<< maxSize << " MAX" << std::endl
-		<< threshold << " THRESH" << std::endl
-		<< printOut << " OUT: " << inFilename << " NAME" << std::endl
-		<< writeOut << " WRITE: " << asa.getLargestSize() << std::endl;
 	
-
-
-	//177/178 for chess
-
-
-	std:: cout << "==========" << endl;
-	asa.writeComponents("sample_out.pgm");
-	std:: cout << asa.getLargestSize()<< endl;
-	std:: cout << asa.getSmallestSize() << endl;
-	std::cout << "Num1: " << asa.getComponentCount() << endl;
-
-	asa.filterComponentsBySize(5000, 7500);
-	std:: cout << "==========" << endl;
-	asa.writeComponents("sample_fileter_out.pgm");
-
-	std:: cout << asa.getLargestSize()<< endl;
-	std:: cout << asa.getSmallestSize() << endl;
-	std::cout << "Num2: " << asa.getComponentCount() << endl;
-
-	for (int i = 0; i < asa.getComponentCount(); ++i)
-	{
-		std::cout << asa.getComponent(i).getBounds() << "das ";
-		asa.printComponentData(asa.getComponent(i));
-		
-	}
-	
-	//std::multiset<std::shared_ptr<ConnectedComponent>, compareComponents> bee = asa.getComponents();
-	//auto it = bee.begin();
-	//asa.printComponentData(*(*it));
-	
-
-
 }
